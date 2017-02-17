7db133c89173b6a1386a21c47e9df48a68bc50e5
Melvyn Harbour <melharbour@gmail.com>
2017-02-17 16:56:21 +0000

    Add FileService

---
+++ TimeNetSync/TimeNetSync/Services/FileService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeNetSync.Services
+{
+    public class FileService : IFileService
+    {
+        public string OpenFileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".timeNetCompetition";
+            openFileDialog.Filter = "Time.NET Files (*.timeNetCompetition)|*.timeNetCompetition";
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments), "Time.NET 2\\Competitions");
+
+            if (openFileDialog.ShowDialog() == true)
+                return openFileDialog.FileName;
+            else
+                return string.Empty;
+        }
+    }
+}
