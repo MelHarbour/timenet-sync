a9f08889391551583740b5482dd4177767efd981
Melvyn Harbour <melharbour@gmail.com>
2017-02-28 13:41:40 +0000

    Set up test project

---
+++ TimeNetSync/TimeNetSyncTests/CompetitorListTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeNetSync.ViewModel;
+
+namespace TimeNetSyncTests
+{
+    [TestClass]
+    public class CompetitorListTests
+    {
+        [TestMethod]
+        public void ToggleConnectCommand_ShouldToggleIsConnecting()
+        {
+            CompetitorListViewModel viewModel = new CompetitorListViewModel();
+
+            bool isConnecting = viewModel.IsConnecting;
+            viewModel.ToggleConnectingCommand.Execute(null);
+            Assert.AreNotEqual(isConnecting, viewModel.IsConnecting);
+        }
+    }
+}
---
+++ TimeNetSync/TimeNetSyncTests/CompetitorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeNetSync.Model;
+
+namespace TimeNetSyncTests
+{
+    [TestClass]
+    public class CompetitorTests
+    {
+        private Competitor competitor;
+        private MultisportResult resultZero;
+        private MultisportResult resultOne;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            competitor = new Competitor();
+            resultZero = new MultisportResult();
+            resultZero.Section = 0;
+            resultZero.TimeOfDay = new TimeSpan(7, 0, 0);
+            competitor.Results.Add(resultZero);
+
+            resultOne = new MultisportResult();
+            resultOne.Section = 1;
+            resultOne.TimeOfDay = new TimeSpan(7, 30, 0);
+            competitor.Results.Add(resultOne);
+        }
+
+        [TestMethod]
+        public void StartTime_ShouldReturnTimeForSegmentZero()
+        {
+            Assert.AreSame(resultZero, competitor.StartTime);
+        }
+
+        [TestMethod]
+        public void FinishTime_ShouldReturnTimeForSegmentOne()
+        {
+            Assert.AreSame(resultOne, competitor.FinishTime);
+        }
+
+        [TestMethod]
+        public void RunTime_ShouldReturnDifferenceBetweenStartAndFinishTimes()
+        {
+            Assert.AreEqual(new TimeSpan(0, 30, 0), competitor.RunTime);
+        }
+    }
+}
