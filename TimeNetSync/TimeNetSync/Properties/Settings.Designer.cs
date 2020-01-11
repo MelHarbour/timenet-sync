2954105e58077a7e9164ef18e22b40e93c5c2498
Mel Harbour <melharbour@gmail.com>
2020-01-11 21:14:05 +0000

    Remove default paths

--- TimeNetSync/TimeNetSync/Properties/Settings.Designer.cs
+++ TimeNetSync/TimeNetSync/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TimeNetSync.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -61,7 +61,7 @@ namespace TimeNetSync.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\Public\\Documents\\Time.NET 2\\Competitions\\WEHoRR 2018.timeNetCompetition")]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string TimeNetFileLocation {
             get {
                 return ((string)(this["TimeNetFileLocation"]));
